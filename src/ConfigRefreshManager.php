247a06e182789d8d5542c6ea9f5a9ad36219285b
Vijayachandran Mani <vijaycs85@gmail.com>
2014-11-28 18:32:33 +0000

    Initial commit.

---
+++ src/ConfigRefreshManager.php
@@ -0,0 +1,198 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\config_refresh\ConfigRefreshManager
+ */
+
+namespace Drupal\config_refresh;
+
+use Drupal\Core\Config\ConfigManagerInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Extension\Extension;
+use Drupal\Core\Extension\ExtensionDiscovery;
+use Drupal\Core\Config\Config;
+use Drupal\Core\Config\InstallStorage;
+use Drupal\Core\Config\FileStorage;
+
+/**
+ * Class ConfigRefreshManager
+ *
+ * @package Drupal\config_refresh
+ */
+Class ConfigRefreshManager {
+
+  protected $configManager;
+
+  protected $entityManager;
+
+  public function __construct(ConfigManagerInterface $config_manager, EntityManagerInterface $entity_manager) {
+    $this->configManager = $config_manager;
+    $this->entityManager =  $entity_manager;
+  }
+
+
+  /**
+   * @param $module_name
+   * @param string $config_type
+   */
+  public function refresh($module_name, $config_type) {
+    $config_entity_type = \Drupal::entityManager()->getDefinition($config_type);
+    $entity_storage = \Drupal::entityManager()->getStorage($config_type);
+    $modules = $this->getModules();
+    $class_name = get_class($this);
+
+    if ($module_name == 'all') {
+      foreach ($modules as $module) {
+        $operations[] = array(array($class_name, 'updateConfigEntity'), array($module, $entity_storage, $config_entity_type));
+        // $this->updateConfigEntity($module, $entity_storage, $config_entity_type);
+      }
+    }
+    elseif (isset($modules[$module_name])) {
+      $operations[] = array(array($class_name, 'updateConfigEntity'), array($modules[$module_name], $entity_storage, $config_entity_type));
+      // $this->updateConfigEntity($modules[$module_name], $entity_storage, $config_entity_type);
+    }
+
+    if (!empty($operations)) {
+      $batch = array(
+        'operations'    => $operations,
+        'title'         => t('Refreshing configuration'),
+        'progress_message' => '',
+        'error_message' => t('Error refreshing/updating configuration'),
+      );
+      batch_set($batch);
+    }
+  }
+
+  public function refreshById($module_name, $config_type, $config_id) {
+    $config_entity_type = \Drupal::entityManager()->getDefinition($config_type);
+    $entity_storage = \Drupal::entityManager()->getStorage($config_type);
+    $modules = $this->getModules();
+    $module = $modules[$module_name];
+    $extension_path = $module->getPath();
+
+    if (is_dir($extension_path . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY)) {
+      $install_storage = new FileStorage($extension_path . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY);
+      $loaded_entity = $entity_storage->load($config_id);
+      $data = $loaded_entity->toArray();
+      unset($data['uuid']);
+      $config = new Config($config_entity_type->getConfigPrefix() . '.' . $config_id, $install_storage, \Drupal::service('event_dispatcher'), \Drupal::service('config.typed'));
+      $config->setData($data)->save();
+
+    }
+  }
+
+
+  /**
+   * @param Extension $module
+   * @param EntityStorageInterface $storage
+   * @param EntityTypeInterface $type
+   */
+  public function updateConfigEntity(Extension $module, EntityStorageInterface $storage, EntityTypeInterface $type) {
+    $extension_path = $module->getPath();
+    // If the extension provides configuration schema clear the definitions.
+    if (is_dir($extension_path . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY)) {
+      $install_storage = new FileStorage($extension_path . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY);
+      $entities = $install_storage->listAll($type->getConfigPrefix());
+      if (count($entities)) {
+        // Ensure module is installed.
+        \Drupal::service('module_installer')->install(array($module->getName()));
+      }
+      foreach ($entities as $name) {
+        $id = substr($name, strlen($type->getConfigPrefix()) + 1);
+        $loaded_entity = $storage->load($id);
+        $data = $loaded_entity->toArray();
+        unset($data['uuid']);
+        $config = new Config($name, $install_storage, \Drupal::service('event_dispatcher'), \Drupal::service('config.typed'));
+        $config->setData($data)->save();
+      }
+    }
+
+    if (FALSE) {
+      $this->updateTestConfigEntity($extension_path, $module, $storage, $type);
+    }
+  }
+
+  public function findConfigurationTypes($module_name) {
+    $config_types = array();
+    $modules = $this->getModules();
+    $module = $modules[$module_name];
+    $extension_path = $module->getPath();
+    // If the extension provides configuration schema clear the definitions.
+    if (is_dir($extension_path . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY)) {
+      $install_storage = new FileStorage($extension_path . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY);
+      $config_list = $install_storage->listAll();
+      if (count($config_list)) {
+        foreach ($config_list as $config_name) {
+          // Handle config entities.
+          if ($entity_type_id = $this->configManager->getEntityTypeIdByName($config_name)) {
+            $entity_type = $this->configManager->getEntityManager()->getDefinition($entity_type_id);
+            $config_types[$entity_type_id] = $entity_type->getLabel();
+          }
+        }
+      }
+      return $config_types;
+    }
+  }
+
+  public function getEntityIds($module_name, $config_type) {
+    $entity_ids = array();
+    $modules = $this->getModules();
+    $module = $modules[$module_name];
+    $extension_path = $module->getPath();
+    $definitions = $this->entityManager->getDefinitions();
+    // If the extension provides configuration schema clear the definitions.
+    if (is_dir($extension_path . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY)) {
+      $install_storage = new FileStorage($extension_path . '/' . InstallStorage::CONFIG_INSTALL_DIRECTORY);
+      $config_list = $install_storage->listAll();
+      if (count($config_list)) {
+        foreach ($config_list as $config_name) {
+          // Handle config entities.
+          if ($entity_type_id = $this->configManager->getEntityTypeIdByName($config_name)) {
+            if ($entity_type_id == $config_type) {
+              $id = substr($config_name, strlen($definitions[$entity_type_id]->getConfigPrefix()) + 1);
+              $entity_ids[] = $id;
+            }
+          }
+        }
+      }
+      return $entity_ids;
+    }
+  }
+
+  protected function getModules() {
+    $listing = new ExtensionDiscovery(\Drupal::root());
+    $modules = $listing->scan('module');
+    return $modules;
+  }
+
+  protected function updateTestConfigEntity($extension_path, Extension $module, EntityStorageInterface $entity_storage, EntityTypeInterface $config_entity_type) {
+    if (is_dir($extension_path . '/test_views')) {
+      // Ensure module is installed.
+      \Drupal::service('module_installer')->install(array($module->getName()));
+      $install_storage = new  FileStorage($extension_path . '/test_views');
+      $entities = $install_storage->listAll($config_entity_type->getConfigPrefix());
+      foreach ($entities as $name) {
+        if (\Drupal::service('config.storage')->exists($name)) {
+          $id = substr($name, strlen($config_entity_type->getConfigPrefix()) + 1);
+          $entity_storage->load($id)->delete();
+        }
+        $entity = $entity_storage->createFromStorageRecord($install_storage->read($name));
+        try {
+          $entity->save();
+        }
+        catch (\Exception $e) {
+          print "Saving $name has generated the following exception: " . $e->getMessage() . "\n";
+          continue;
+        }
+        $data = $entity->toArray();
+        unset($data['uuid']);
+        $config = new Config($name, $install_storage, \Drupal::service('event_dispatcher'), \Drupal::service('config.typed'));
+        $config->setData($data)->save();
+      }
+    }
+  }
+
+}
---
+++ src/Controller/ConfigRefreshController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\config_refresh\Controller\ConfigRefreshController
+ */
+
+namespace Drupal\config_refresh\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+
+class ConfigRefreshController extends ControllerBase {
+
+
+  public function overview() {
+
+  }
+
+}
